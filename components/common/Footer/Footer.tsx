199a330dc919e4212fb1235a932ee7ba5dd7ca27
Hayoung <gkduddy0814@gmail.com>
2021-06-17 00:50:24 +0900

    feat : add footer components

---
+++ components/common/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import cn from 'classnames';
+
+interface Props {
+  className?: string;
+}
+
+const Footer: FC<Props> = ({ className }) => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
