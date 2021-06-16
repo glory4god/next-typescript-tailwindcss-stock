f9caa1ba71dc1fb7b00363476ff5f43efd2a2d3c
Hayoung <gkduddy0814@gmail.com>
2021-06-17 00:53:07 +0900

    feat : add Container components

---
+++ components/ui/Container/Container.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import cn from 'classnames';
+
+interface ContainerProps {
+  className?: string;
+  children?: any;
+}
+
+const Container: FC<ContainerProps> = ({ children, className }) => {
+  return (
+    <div
+      className={
+        (cn(className),
+        'min-h-screen max-w-screen-lg text-center pt-20 text-black mx-auto px-6')
+      }>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
