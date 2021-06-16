c1d6b4ac8aba0d10049ec7715d9e00a17ff024e6
Hayoung <gkduddy0814@gmail.com>
2021-06-17 00:51:41 +0900

    feat : add subNavbar components feature

---
+++ components/common/Subnavbar/SubNavbar.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import Link from 'next/link';
+import Button from '@material-ui/core/Button';
+
+interface Props {
+  className?: string;
+  pages: {
+    main: string;
+    sub: { first: string; second: string; third: string; forth?: string };
+  };
+}
+
+const SubNavbar: FC<Props> = ({ className, pages }) => {
+  return (
+    <div className="text-xs space-x-6">
+      <Link href={`/${pages.main}/${pages.sub.first}`}>
+        <a>
+          <Button>{pages.sub.first}</Button>
+        </a>
+      </Link>
+      <Link href={`/${pages.main}/${pages.sub.second}`}>
+        <a>
+          <Button>{pages.sub.second}</Button>
+        </a>
+      </Link>
+      <Link href={`/${pages.main}/${pages.sub.third}`}>
+        <a>
+          <Button>{pages.sub.third}</Button>
+        </a>
+      </Link>
+      {pages.sub.forth && (
+        <Link href={`/${pages.main}/${pages.sub.forth}`}>
+          <a>
+            <Button>{pages.sub.forth}</Button>
+          </a>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default SubNavbar;
