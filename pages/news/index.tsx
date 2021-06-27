4cf93bb2d3c9105c37bbe7b617d50f6ba9fc96b0
Hayoung <gkduddy0814@gmail.com>
2021-06-27 22:56:43 +0900

    feat : add newsp pages

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -2,15 +2,49 @@ import React from 'react';
 import PopularViews from '../../components/news/PopularViews';
 import SearchBar from '../../components/news/SearchBar';
 import Head from 'next/head';
+import Container from '../../components/ui/Container';
 
-export default function News() {
+import { GetServerSideProps } from 'next';
+import fetcher from '../../lib/fetcher';
+import TopicContainer from '../../components/news/Topic';
+
+export default function News({ topic }: { topic: Array<string> }) {
   return (
-    <div className="min-h-screen max-w-screen-lg text-center pt-20 text-black mx-auto md:flex md:space-x-4 px-6">
+    <Container>
       <Head>
         <title>TITLE-NEWS</title>
       </Head>
-      <SearchBar className="md:w-3/4 mt-6 h-11 relative" />
-      <PopularViews className="md:w-60 mt-6 space-y-2" />
-    </div>
+      <div className="mt-6 md:flex md:space-x-4">
+        <div className="md:w-3/4">
+          <SearchBar className=" h-11" topic={topic} />
+          <TopicContainer className="my-4" topic={topic} />
+        </div>
+        <div className="md:w-1/4 ">
+          <PopularViews className=" space-y-2" />
+        </div>
+      </div>
+    </Container>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = (await fetcher(
+    'http://localhost:8080/api/v1/news/pop-keyword',
+  )) as Array<string>;
+
+  const topic = res.filter((arr, idx) => {
+    if (idx < 10) {
+      return arr;
+    }
+  });
+
+  // const autoKeyword = (await fetcher(
+  //   'http://localhost:8080/api/v1/news/pop-keyword',
+  // )) as Array<string>;
+
+  return {
+    props: {
+      topic,
+    },
+  };
+};
