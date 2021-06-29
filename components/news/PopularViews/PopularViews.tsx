f895d639cbb3871faeb6823cf96dc66ac9e51052
Hayoung <gkduddy0814@gmail.com>
2021-06-29 11:39:40 +0900

    fix : change state naming

--- components/news/PopularViews/PopularViews.tsx
+++ components/news/PopularViews/PopularViews.tsx
@@ -10,67 +10,73 @@ interface Props {
   className?: string;
 }
 
+type Menu = {
+  keyword: boolean;
+  news: boolean;
+};
+
+type SearchCondition = {
+  daily: boolean;
+  weekly: boolean;
+};
+
 const PopularViews: React.FC<Props> = ({ className }) => {
-  const [isClicked, setIsClicked] = React.useState({
+  const [menu, setMenu] = React.useState<Menu>({
     keyword: false,
     news: false,
   });
 
-  const clickPopularKeyword = () => {
-    setIsClicked(() => ({
-      keyword: !isClicked.keyword,
+  const clickKeyword = () => {
+    setMenu(() => ({
+      keyword: !menu.keyword,
       news: false,
     }));
   };
-  const clickPopularNews = () => {
-    setIsClicked(() => ({
+  const clickNews = () => {
+    setMenu(() => ({
       keyword: false,
-      news: !isClicked.news,
+      news: !menu.news,
     }));
   };
   return (
     <div className={cn(className)}>
       <Paper
-        className={`${isClicked.keyword === true ? 'max-h-96' : 'h-12'} pt-3`}
+        className={`${menu.keyword === true ? 'max-h-96' : 'h-12'} pt-3`}
         elevation={3}>
-        <h2 className="cursor-pointer" onClick={() => clickPopularKeyword()}>
+        <h2 className="cursor-pointer" onClick={() => clickKeyword()}>
           Popular Keyword
-          {!isClicked.keyword ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
+          {!menu.keyword ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
         </h2>
         <Paper elevation={4}>
           <div
             className={`${
-              isClicked.keyword === true ? 'max-h-72' : 'max-h-0'
-            } transition:max-height duration-300 ease-out h-72 mt-1`}>
-            {isClicked.keyword && (
+              menu.keyword === true ? 'max-h-80' : 'max-h-0'
+            } transition:max-height duration-300 ease-out mt-1`}>
+            {menu.keyword && (
               <PopularKeywordBox
-                className="px-4 py-2"
-                failed={() =>
-                  setIsClicked(() => ({ ...isClicked, keyword: false }))
-                }
+                className="px-3 py-2"
+                failed={() => setMenu(() => ({ ...menu, keyword: false }))}
               />
             )}
           </div>
         </Paper>
       </Paper>
       <Paper
-        className={`${isClicked.news === true ? 'max-h-96' : 'h-12'} pt-3`}
+        className={`${menu.news === true ? 'max-h-96' : 'h-12'} pt-3`}
         elevation={3}>
-        <h2 className="cursor-pointer" onClick={() => clickPopularNews()}>
+        <h2 className="cursor-pointer" onClick={() => clickNews()}>
           Popular News
-          {!isClicked.news ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
+          {!menu.news ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
         </h2>
         <Paper elevation={4}>
           <div
             className={`${
-              isClicked.news === true ? 'max-h-80' : 'max-h-0'
-            } transition:max-height duration-300 ease-out md:h-80 h-52 mt-1`}>
-            {isClicked.news && (
+              menu.news === true ? 'max-h-96' : 'max-h-0'
+            } transition:max-height duration-300 ease-out mt-1`}>
+            {menu.news && (
               <PopularNewsBox
-                className="px-3 pt-2 h-full grid grid-cols-1"
-                failed={() =>
-                  setIsClicked(() => ({ ...isClicked, news: false }))
-                }
+                className="px-2 pt-2 "
+                failed={() => setMenu(() => ({ ...menu, news: false }))}
               />
             )}
           </div>
