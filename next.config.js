dde4d0cdf837a4d2e61f813db64bad537e8630cc
Hayoung <gkduddy0814@gmail.com>
2021-07-05 18:06:44 +0900

    feat : register Image tag

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['imgnews.pstatic.net'],
+    domains: [
+      'imgnews.pstatic.net',
+      'ssl.pstatic.net',
+      'cdn.incheonilbo.com',
+      'cgeimage.commutil.kr',
+      'mimgnews.pstatic.net',
+    ],
   },
 };
