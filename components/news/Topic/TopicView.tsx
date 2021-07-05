3d34678ec2a8ab8e3200f2e1d0c0257c5e24a72f
Hayoung <gkduddy0814@gmail.com>
2021-07-05 17:36:33 +0900

    feat : add save url in clipboard and image

--- components/news/Topic/TopicView.tsx
+++ components/news/Topic/TopicView.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 import LinkIcon from '@material-ui/icons/Link';
+import copy from 'copy-to-clipboard'; // url 클립보드 복사 기능
+import styles from './Topic.module.css';
 
 interface Props {
   topic: string;
   link: string;
   title: string;
-  description: string;
+  imageUrl: string;
   skeleton: boolean;
 }
 
@@ -14,37 +16,61 @@ const TopicView: React.FC<Props> = ({
   topic,
   link,
   title,
-  description,
+  imageUrl,
   skeleton,
 }) => {
   return (
     <div
       style={{ width: '328px', height: '400px' }}
-      className="border-2 rounded-xl px-4 my-2">
-      <div className="text-xl flex justify-between text-left px-4 font-bold py-8">
+      className="border-2 rounded-xl my-2">
+      <div className="text-xl flex justify-between text-left px-8 font-bold py-8 ">
         <span className="bg-indigo-200 rounded-3xl text-xs text-white px-4 py-1">
           # {topic}
         </span>
         <LinkIcon
           onClick={() => {
+            // url 클립보드 복사 기능
             if (skeleton === false) {
-              var win = window.open(link);
-              if (win !== null) {
-                win.focus();
-              }
+              copy(link);
+              alert('copied url link!');
             }
+            // if (skeleton === false) {
+            //   var win = window.open(link);
+            //   if (win !== null) {
+            //     win.focus();
+            //   }
+            // }
           }}
-          className="cursor-pointer"
+          className="cursor-pointer transform rotate-45"
         />
       </div>
       <h4
-        className="h-28 text-left text-lg"
+        className="h-24 text-left text-xl px-4"
         dangerouslySetInnerHTML={{ __html: title }}
       />
-      <p dangerouslySetInnerHTML={{ __html: description }} />
-      {/* <Image src={link} alt={title} /> */}
+      <div
+        className="cursor-pointer"
+        onClick={() => {
+          if (skeleton === false) {
+            var win = window.open(link);
+            if (win !== null) {
+              win.focus();
+            }
+          }
+        }}>
+        {imageUrl !== 'nothing' ? (
+          <Image
+            className={styles.image}
+            src={`${imageUrl}`}
+            alt={title}
+            width={328}
+            height={215}></Image>
+        ) : (
+          <Image src={'/favicon.ico'} alt={title} width={328} height={210} />
+        )}
+      </div>
     </div>
   );
 };
 
-export default TopicView;
+export default React.memo(TopicView);
