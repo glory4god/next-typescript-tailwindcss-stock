a56663084b42697ea36d9cef7490c6fd04ef1483
Hayoung <gkduddy0814@gmail.com>
2021-07-05 17:34:56 +0900

    fix : change components file

--- components/news/index.ts
+++ components/news/index.ts
@@ -1,3 +1,4 @@
 export { default as NewsSearchList } from './NewsSearchList';
 export { default as PopularViews } from './PopularViews';
 export { default as SearchBar } from './SearchBar';
+export { default as Topic } from './Topic';
