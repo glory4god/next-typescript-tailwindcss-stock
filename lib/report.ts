755440ede552f3486ae88c9fbe9761fab652b2da
Hayoung <gkduddy0814@gmail.com>
2021-07-08 01:58:08 +0900

    feat : add handlers

--- lib/report.ts
+++ lib/report.ts
@@ -3,7 +3,7 @@ import type { ChartReport } from '../types/report/ReportType';
 
 export async function getReportIds() {
   const report = (await fetcher(
-    'http://54.180.68.136:8080/api/v1/user/chart-report',
+    process.env.AWS_SERVER + 'api/v1/user/chart-report',
   )) as Array<ChartReport>;
 
   const paths: string[] = report.map((arr) => {
@@ -13,8 +13,40 @@ export async function getReportIds() {
 }
 
 export async function getReportById(id: string) {
-  const data = (await fetcher(
-    `http://54.180.68.136:8080/api/v1/user/chart-report/${id}`,
+  return (await fetcher(
+    process.env.AWS_SERVER + `api/v1/user/chart-report/${id}`,
   )) as ChartReport;
-  return data;
+}
+
+export async function goodHandler(id: number, value: string) {
+  return (await fetcher(
+    process.env.AWS_SERVER +
+      `api/v1/user/chart-report/good/${id}?value=${value}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+  )) as string;
+}
+
+export async function badHandler(id: number, value: string) {
+  return (await fetcher(
+    process.env.AWS_SERVER +
+      `api/v1/user/chart-report/bad/${id}?value=${value}`,
+    {
+      method: 'PATCH',
+    },
+  )) as string;
+}
+
+export async function viewsHandler(id: number) {
+  return (await fetcher(
+    process.env.AWS_SERVER + `api/v1/user/chart-report/views/${id}`,
+    {
+      method: 'PATCH',
+    },
+  )) as string;
 }
