6cf2e04635a93cd61a57fd3319df8e20053a095b
Hayoung <gkduddy0814@gmail.com>
2021-07-08 01:53:49 +0900

    feat : add footer components

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react';
-import cn from 'classnames';
 
 interface Props {
   className?: string;
@@ -7,8 +6,21 @@ interface Props {
 
 const Footer: FC<Props> = ({ className }) => {
   return (
-    <div>
-      <div></div>
+    <div className="h-40 md:pt-8 pt-4 mt-8 text-black my-auto px-6 bg-gray-100">
+      <div className="max-w-screen-lg mx-auto">
+        <div>
+          <b>Yu</b>
+        </div>
+        <div>2021.5 ~ now</div>
+        <div>
+          <b>front-end</b> : Nextjs / Reactjs / Typescript / Tailwindcss /
+          Library(recharts / material UI / Responsive Web)
+        </div>
+        <div>
+          <b>back-end</b> : Java Springboot / MariaDB / AWS EC2 / AWS RDS /
+          Naver News API
+        </div>
+      </div>
     </div>
   );
 };
