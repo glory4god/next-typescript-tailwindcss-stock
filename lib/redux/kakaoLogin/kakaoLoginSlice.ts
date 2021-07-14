a78b8ea55dc4711510f680a027e437123e392942
Hayoung <gkduddy0814@gmail.com>
2021-07-14 23:01:21 +0900

    feat : add login redux slice

--- lib/redux/kakaoLogin/kakaoLoginSlice.ts
+++ lib/redux/kakaoLogin/kakaoLoginSlice.ts
@@ -1,19 +1,65 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import fetcher from '../../fetcher';
 import { RootState } from '../store';
 
-export interface KakaoLoginProps {}
+export interface KakaoLoginProps {
+  loading: boolean;
+  access_token: string;
+}
 
-const initialState: KakaoLoginProps = {};
+const initialState: KakaoLoginProps = {
+  loading: false,
+  access_token: '',
+};
 
 export const kakaoLoginSlice = createSlice({
   name: 'kakaologin',
   initialState,
   reducers: {
-    kakaoLogin: (state, action: PayloadAction<string>) => {},
+    login: (state) => {
+      state.loading = true;
+    },
+    loginSuccess: (state, action: PayloadAction<string>) => {
+      state.access_token = action.payload;
+      state.loading = false;
+    },
+    loginFail: (state) => {
+      state.loading = false;
+    },
   },
 });
 
-export const { kakaoLogin } = kakaoLoginSlice.actions;
-export const selectKakaoLogin = (state: RootState) => state.kakaologin;
+export const { login, loginSuccess, loginFail } = kakaoLoginSlice.actions;
+export const selectKakaoLogin = (state: RootState) => state.login;
 
 export default kakaoLoginSlice.reducer;
+
+export function getAccessToken(code: string) {
+  return async (dispatch: any) => {
+    dispatch(login());
+    try {
+      const tokenData = await getToken(code);
+      dispatch(loginSuccess(tokenData.access_token));
+      localStorage.setItem('token', tokenData.access_token);
+      alert('로그인에 성공했습니다!');
+    } catch (error) {
+      dispatch(loginFail());
+      alert('로그인에 실패했습니다!');
+    }
+  };
+}
+
+export const getToken = async (code: string | null) => {
+  return (await fetcher(
+    `http://localhost:8080/api/v2/login/callback/kakao?code=${code}`,
+  )) as TokenData;
+};
+
+export type TokenData = {
+  access_token: string;
+  token_type: string;
+  refresh_token: string;
+  expires_in: string;
+  scope: string;
+  refresh_token_expires_in: string;
+};
