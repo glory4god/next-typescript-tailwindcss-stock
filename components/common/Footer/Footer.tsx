5d4b7c0ffb6bfac8bed31ede642c04c7160cfbaa
Hayoung <gkduddy0814@gmail.com>
2021-07-16 23:48:52 +0900

    feat : add stack in footer

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -10,15 +10,16 @@ const Footer: React.FC<Props> = ({ className }) => {
     <div
       className={cn(
         className,
-        'h-36 md:pt-8 pt-4 mt-8 text-black my-auto px-6 bg-gray-100',
+        'h-44 md:pt-8 pt-4 mt-8 text-black my-auto px-6 bg-gray-100',
       )}>
       <div className="max-w-screen-lg mx-auto">
         <div className="pb-4">
           <b>Project name</b> <span>2021.5 ~ now</span>
         </div>
         <div>
-          <b>front-end</b> : Nextjs / Reactjs / Typescript / Tailwindcss / CSS
-          Modules / Library [ recharts / material UI / Responsive Web ]
+          <b>front-end</b> : Nextjs / Reactjs / Typescript / React-Redux /
+          Redux-toolkit/ Tailwindcss / CSS Modules / Library [ recharts /
+          material UI / Responsive Web ]
         </div>
         <div>
           <b>back-end</b> : Java Springboot / MariaDB / AWS EC2 / AWS RDS /
