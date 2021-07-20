9fb6023f54ef03c58e364f17116da5295b3f38fe
Hayoung <gkduddy0814@gmail.com>
2021-07-20 23:37:19 +0900

    fix : combine good and bad handler

--- lib/report.ts
+++ lib/report.ts
@@ -3,7 +3,8 @@ import type { ChartReport } from '../types/report/ReportType';
 
 export async function getReportIds() {
   const report = (await fetcher(
-    process.env.AWS_SERVER + 'api/v1/user/chart-report',
+    process.env.LOCAL_SERVER +
+      'api/v1/user/chart-report/sort-all?sorted=modifiedDate',
   )) as Array<ChartReport>;
 
   const paths: string[] = report.map((arr) => {
@@ -14,37 +15,30 @@ export async function getReportIds() {
 
 export async function getReportById(id: string) {
   return (await fetcher(
-    process.env.AWS_SERVER + `api/v1/user/chart-report/${id}`,
+    process.env.LOCAL_SERVER + `api/v1/user/chart-report/${id}`,
   )) as ChartReport;
 }
 
-export async function goodHandler(id: number, value: string) {
+export async function goodAndBadHandler(
+  value: string,
+  id: number,
+  reportId: number,
+) {
   return (await fetcher(
-    process.env.AWS_SERVER +
-      `api/v1/user/chart-report/good/${id}?value=${value}`,
+    process.env.LOCAL_SERVER + `api/v1/user/chart-report/patch/${value}`,
     {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
       },
-    },
-  )) as string;
-}
-
-export async function badHandler(id: number, value: string) {
-  return (await fetcher(
-    process.env.AWS_SERVER +
-      `api/v1/user/chart-report/bad/${id}?value=${value}`,
-    {
-      method: 'PATCH',
+      body: JSON.stringify({ userId: id, reportId: reportId }),
     },
   )) as string;
 }
 
 export async function viewsHandler(id: number) {
   return (await fetcher(
-    process.env.AWS_SERVER + `api/v1/user/chart-report/views/${id}`,
+    process.env.LOCAL_SERVER + `api/v1/user/chart-report/views/${id}`,
     {
       method: 'PATCH',
     },
