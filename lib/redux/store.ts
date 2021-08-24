9d4d30d9b0d811c4cc5c5a97f1410c592b20f41a
Hayoung <gkduddy0814@gmail.com>
2021-08-24 14:25:16 +0900

    feat : add freeBoard reducer in store

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reportSlice from './report/reportSlice';
 import kakaoLoginSlice from './kakaoLogin/kakaoLoginSlice';
+import freeBoardSlice from './freeBoard/freeBoardSlice';
 
 export const store = configureStore({
   reducer: {
     report: reportSlice,
     login: kakaoLoginSlice,
+    freeBoard: freeBoardSlice,
   },
 });
 
