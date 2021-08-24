6a89494ecd686defcd676f2a20cdde0a5a84ab68
Hayoung <gkduddy0814@gmail.com>
2021-08-24 14:19:42 +0900

    feat : add recycle boardList components

---
+++ components/board/BoardList/BoardList.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import cn from 'classnames';
+import type { ChartReport, FreeBoard } from '../../../types/report/ReportType';
+import BoardContent from '../ReportBoardContent';
+import Button from '@material-ui/core/Button';
+import FreeBoardContent from '../FreeBoardContent';
+
+interface Props {
+  reportList?: ChartReport[];
+  freeBoardList?: FreeBoard[];
+  listNumber: number;
+}
+
+const BoardList: React.FC<Props> = ({
+  reportList,
+  freeBoardList,
+  listNumber,
+}) => {
+  const [currentBoardPage, setCurrentBoardPage] = React.useState<number>(1);
+
+  let boardPages: number = 0;
+  if (reportList !== undefined) {
+    boardPages = Math.ceil(reportList.length / listNumber);
+  } else if (freeBoardList !== undefined) {
+    boardPages = Math.ceil(freeBoardList.length / listNumber);
+  }
+
+  const arr: Array<number> = [];
+  for (var t = 1; t <= boardPages; t++) {
+    arr.push(t);
+  }
+
+  React.useEffect(() => {
+    setCurrentBoardPage(1);
+  }, [reportList]);
+  return (
+    <>
+      <div
+        style={{
+          height: '100%',
+        }}>
+        {reportList !== undefined &&
+          reportList?.map((arr, idx) => {
+            if (
+              idx >= (currentBoardPage - 1) * listNumber &&
+              idx < currentBoardPage * listNumber
+            )
+              return <BoardContent key={arr.username + idx} item={arr} />;
+          })}
+        {freeBoardList !== undefined &&
+          freeBoardList?.map((arr, idx) => {
+            if (
+              idx >= (currentBoardPage - 1) * listNumber &&
+              idx < currentBoardPage * listNumber
+            )
+              return <FreeBoardContent key={arr.username + idx} item={arr} />;
+          })}
+      </div>
+      <div className="mt-6 text-center flex justify-between items-center">
+        <Button
+          onClick={() => {
+            if (currentBoardPage !== 1) {
+              setCurrentBoardPage((c) => c - 1);
+            } else {
+              alert('이전 페이지가 없습니다!');
+            }
+          }}>
+          {'< '} 이전
+        </Button>
+        <div>
+          {arr.map((arr) => {
+            // listNumber으로 나눈 몫이 같은 값들을 추출 (이렇게하면 같은 자리수 목록 추출 가능)
+            if (Math.floor(arr / 6) === Math.floor(currentBoardPage / 6)) {
+              return (
+                <button
+                  key={'downButton' + arr}
+                  onClick={() => {
+                    setCurrentBoardPage(arr);
+                  }}
+                  className={cn('px-3 border-2 rounded-md border-indigo-200 ', {
+                    'bg-indigo-400 text-white': currentBoardPage === arr,
+                    'text-indigo-400': currentBoardPage !== arr,
+                  })}>
+                  {arr}
+                </button>
+              );
+            }
+          })}
+        </div>
+        <Button
+          onClick={() => {
+            if (currentBoardPage !== boardPages) {
+              setCurrentBoardPage((c) => c + 1);
+            } else {
+              alert('다음 페이지가 없습니다!');
+            }
+          }}>
+          다음 {' >'}
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default React.memo(BoardList);
---
+++ components/board/index.ts
@@ -0,0 +1,4 @@
+export { default as BoardContent } from './ReportBoardContent';
+export { default as BoardList } from './BoardList';
+export { default as ReportBoardView } from './ReportBoardView';
+export { default as BoardSearchBar } from './BoardSearchBar';
