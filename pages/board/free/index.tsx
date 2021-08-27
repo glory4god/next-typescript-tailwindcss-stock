30d24d67367aaece03cc6c516bf6abe9209b1531
Hayoung <gkduddy0814@gmail.com>
2021-08-27 23:34:43 +0900

    feat : add freeboard page

--- pages/board/free/index.tsx
+++ pages/board/free/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Button from '@material-ui/core/Button';
 import Subnavbar from '../../../components/common/Subnavbar';
 import { BoardList } from '../../../components/board';
 import { Container } from '../../../components/ui';
@@ -7,9 +8,10 @@ import {
   fetchFreeBoard,
   selectFreeBoard,
 } from '../../../lib/redux/freeBoard/freeBoardSlice';
+import Link from 'next/link';
 
 const FreeBoardPage = () => {
-  const { loading, freeBoardList } = useSelector(selectFreeBoard);
+  const { freeBoardList } = useSelector(selectFreeBoard);
   const dispatch = useDispatch();
 
   React.useEffect(() => {
@@ -25,6 +27,13 @@ const FreeBoardPage = () => {
         }}
       />
       <h2 className="my-4">FREE BOARD</h2>
+      <div className="text-right">
+        <Link href="/board/free/writing">
+          <a href="#">
+            <Button>글쓰기</Button>
+          </a>
+        </Link>
+      </div>
       <BoardList freeBoardList={freeBoardList} listNumber={10} />
     </Container>
   );
