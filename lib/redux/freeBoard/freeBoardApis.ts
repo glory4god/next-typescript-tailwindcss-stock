b32a04d0b839f5d38b27b6788dd23bde56f6249a
Hayoung <gkduddy0814@gmail.com>
2021-08-27 23:33:48 +0900

    feat : add apis related to freeboard

--- lib/redux/freeBoard/freeBoardApis.ts
+++ lib/redux/freeBoard/freeBoardApis.ts
@@ -6,3 +6,20 @@ export async function getFreeBoardAll(sorted: string) {
     process.env.LOCAL_SERVER + `api/v1/freeboard?sorted=${sorted}`,
   )) as Array<FreeBoard>;
 }
+
+export async function getFreeBoardIds() {
+  const freeBoard = (await fetcher(
+    process.env.LOCAL_SERVER + 'api/v1/freeboard?sorted=modifiedDate',
+  )) as Array<FreeBoard>;
+
+  const paths: string[] = freeBoard.map((arr) => {
+    return arr.id.toString();
+  });
+  return paths;
+}
+
+export async function getFreeBoardById(id: string) {
+  return (await fetcher(
+    process.env.LOCAL_SERVER + `api/v1/freeboard/${id}`,
+  )) as FreeBoard;
+}
